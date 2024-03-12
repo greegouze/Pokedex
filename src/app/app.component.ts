593cd68a4e9bfa08dbafd20ac1200a021308fc64
greegouze <gregory.silvestri45@gmail.com>
2024-03-12 20:07:40 +0100

    eslint && ts rules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
+    <div>
+      <h1 class="text-3xl font-bold underline">Welcome to {{ title }}!</h1>
+      <router-outlet />
+    </div>
   `,
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Pokedex';
